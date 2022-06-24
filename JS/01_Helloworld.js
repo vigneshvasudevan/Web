b9f6bc1c1115f6b60e87ded2abdc166567009aec
vigneshvasudevan <vigneshkutta@gmail.com>
2022-06-24 15:31:05 +0530

    Create 01_Helloworld.js

---
+++ JS/01_Helloworld.js
@@ -0,0 +1,10 @@
+// This is a single line comment in JS code
+
+const message = "Hello world";
+/* 
+    This a multi-line comment in JS.
+*/
+console.log(message);
+
+
+// Ref: https://262.ecma-international.org/5.1/#sec-11.9.6
